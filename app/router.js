48ce27e43c9c1fbe439dc1aa11fecfe474b31282
Kate Leslie <thisizk8@gmail.com>
2017-04-10 15:36:31 -0500

    fixed styles

--- app/router.js
+++ app/router.js
@@ -6,7 +6,7 @@ const Router = Ember.Router.extend({
   rootURL: config.rootURL
 });
 
-Router.map(function() {
+Router.map(function () {
   this.route('register');
   this.route('login');
   this.route('dashboard');
