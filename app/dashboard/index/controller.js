021e77607826244b162842cefeec34b31da58b85
justindmurphy33 <justindmurphy33@gmail.com>
2017-04-13 14:17:02 -0500

    fixed complications from last merge (I hope)

--- app/dashboard/index/controller.js
+++ app/dashboard/index/controller.js
@@ -9,21 +9,6 @@ export default Ember.Controller.extend({
       this.transitionToRoute('login');
     },
 
-    async addItem() {
-      const item = this.store.createRecord('item', this.formValues);
-
-      await item.save();
-
-      this.resetNewItem();
-      this.toggleProperty('isShowingModal');
-      // transitionToRoute needs to be changed to wait for the add item button to be clicked.
-      // This is activating the closeOutlet function.
-    },
-
-    toggleModal() {
-      this.toggleProperty('isShowingModal');
-    },
-
     deleteItem(item) {
       item.destroyRecord();
     },
