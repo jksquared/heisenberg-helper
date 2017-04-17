595071885dc2a43393b25e6960d5bcce2b43c074
Kate Leslie <thisizk8@gmail.com>
2017-04-17 16:42:01 -0500

    styles added to timer

--- app/components/trianglify-canvas/component.js
+++ app/components/trianglify-canvas/component.js
@@ -7,12 +7,12 @@ export default Ember.Component.extend({
     this._super(...arguments);
 
     const pattern = Trianglify({
-      height: 1000,
+      height: 1250,
       width: 2000,
       cell_size: 250,
       seed: '2qc5n',
       variance: '0.97',
-      x_colors: ['#2a454b', '#405952',  '#FF974F', '#9C9B7A', '#d22d2d', '#F54F29', '#902d16'],
+      x_colors: ['#649688', '#405952', '#9C9B7A', '#FF974F', '#F54F29', '#902d16', '#2a454b', '#d22d2d'],
 
 
     });
