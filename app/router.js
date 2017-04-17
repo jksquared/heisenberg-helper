1fc147b1a89ce2bcba4cf394309837e549206305
Kae Anderson <andersonkaem@gmail.com>
2017-04-17 11:13:35 -0500

    edit item

--- app/router.js
+++ app/router.js
@@ -13,7 +13,12 @@ Router.map(function () {
     this.route('add-time', { path: '/item/:item_id/new-time' });
     this.route('item', { path: '/items' }, function () {
       this.route('new');
+      this.route('edit', { path: '/:item_id/edit' });
+          // copied this from Razer Cars
+          // this.route('edit', { path: '/:brand_id/edit' });
     });
+
+    this.route('detail', () => {});
   });
 });
 export default Router;
