ffa980577c422828162a32f075fe564722d7e43d
justindmurphy33 <justindmurphy33@gmail.com>
2017-04-18 15:55:20 -0500

    add component for timer, made timer route, attempting to get it submitting to server

---
+++ app/components/x-stopwatch/component.js
@@ -0,0 +1,58 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+
+  state: 'reset',
+
+  timeElapsed: 0,
+  lapTime: 0,
+
+  isResetState: Ember.computed.equal('state', 'reset'),
+  isRunState: Ember.computed.equal('state', 'run'),
+  isPauseState: Ember.computed.equal('state', 'pause'),
+  isLapState: Ember.computed.equal('state', 'lap'),
+
+  incrementTimeElapsed(startDate) {
+    const currentDate = new Date();
+    if (this.get('isRunState') || this.get('isLapState')) {
+      this.incrementProperty('timeElapsed', currentDate.valueOf() - startDate.valueOf());
+      Ember.run.next(this, 'incrementTimeElapsed', new Date());
+    }
+  },
+
+  actions: {
+    start() {
+      this.set('state', 'run');
+      this.incrementTimeElapsed(new Date());
+    },
+
+    stop() {
+      this.set('state', 'pause');
+    },
+
+    reset() {
+      this.set('state', 'reset');
+      this.set('timeElapsed', 0);
+    },
+
+    pause() {
+      this.set('lapTime', this.get('timeElapsed'));
+      this.set('state', 'lap');
+    },
+
+    resume() {
+      this.set('state', 'run');
+    },
+
+    submit(changeset) {
+      changeset.save();
+      const time = this.store.createRecord('duration', this.formValues);
+
+      time.set('item', this.model);
+
+      return time.save().then(() => {
+        this.transitionToRoute('dashboard');
+      });
+    },
+  },
+});
