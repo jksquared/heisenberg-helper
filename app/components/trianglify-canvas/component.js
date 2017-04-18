9c64a9920ca591e7a9e61ca95cce4bd6a26e6d01
Kate Leslie <thisizk8@gmail.com>
2017-04-18 11:38:45 -0500

    styling modal

--- app/components/trianglify-canvas/component.js
+++ app/components/trianglify-canvas/component.js
@@ -8,11 +8,11 @@ export default Ember.Component.extend({
 
     const pattern = Trianglify({
       height: 1250,
-      width: 2000,
+      width: 1430,
       cell_size: 250,
       seed: '2qc5n',
       variance: '0.97',
-      x_colors: ['#649688', '#405952', '#9C9B7A', '#FF974F', '#F54F29', '#902d16', '#2a454b', '#d22d2d'],
+      x_colors: ['#2a454b', '#649688', '#405952', '#9C9B7A', '#FF974F', '#F54F29', '#902d16', '#2a454b', '#d22d2d'],
 
 
     });
