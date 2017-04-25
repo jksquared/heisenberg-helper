e8641fec9b4e499bc9d3e6be8a98203afea45e11
justindmurphy33 <justindmurphy33@gmail.com>
2017-04-25 12:53:51 -0500

    have timer submitting integers for minutes instead of millis

--- app/components/trianglify-canvas/component.js
+++ app/components/trianglify-canvas/component.js
@@ -5,7 +5,6 @@ export default Ember.Component.extend({
   classNames: ['canvas'],
   didInsertElement() {
     this._super(...arguments);
-    // debugger;
 
     const pattern = Trianglify({
       height: this.element.scrollHeight,
@@ -14,6 +13,7 @@ export default Ember.Component.extend({
       seed: '2qc5n',
       variance: '0.97',
       x_colors: ['#2a454b', '#649688', '#405952', '#9C9B7A', '#FF974F', '#F54F29', '#902d16', '#2a454b', '#d22d2d'],
+      // x_colors: ['#649688', '#FF974F', '#F54F29', '#902d16', '#2a454b', '#d22d2d'],
 
 
     });
